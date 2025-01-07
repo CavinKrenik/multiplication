59ee8f2fd6e7cf06e7ac6c76664633743c385e46
CavinKrenik <cavinkrenik5@icloud.com>
2025-01-07 11:11:24 -0800

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+
+def multiplication(a, b):
+    return (a * b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'multiplication of {a} and {b} is {multiplication(a, b)}')
